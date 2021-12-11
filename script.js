82eb7da9459279d533f544e34b2698c758148fb3
bhumikak08 <69806752+bhumikak08@users.noreply.github.com>
2021-12-11 21:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function validateform(){
     
 }
-        
+function compute()
+{    var principal = document.getElementById("principal").value;
+if(principal<0){
+    alert("Enter positive value!!");
+   
+
+} 
+else if(principal==0){
+    alert("principal cannot be zero!!");
+    
+}
+else {
+   
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result=document.getElementById("result");
+    result.innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span class='highlight'>"+rate+"</span>"+"%\<br\>You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+",\<br\>in the year "+"<span class='highlight'>"+year+"</span>"+"\<br\>";
+    
+}}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval;
+}
+function showVal(newvalue) {
+    document.getElementById(
+      "rate_val").innerHTML = newvalue;
+}
